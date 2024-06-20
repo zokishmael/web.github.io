30b24cfce40458d84f92f305cc26c87f23ec5c9d
zokishmael <59413896+zokishmael@users.noreply.github.com>
2024-06-20 14:26:30 +0900

    Add files via upload

---
+++ js/linkpage-blogger-template-ed.js
@@ -0,0 +1,137 @@
+function _0x4ef1() {
+    var _0x1fb51e = ['&sz=32', '.blogger.com', '<b>Kode Lisensi</b> dibutuhkan..<br><br><small>#ERR_LICENSE</small>', '1244848HYULIS', '.blogspot.com', '225372ZpJnUb', '//m.', '<b>Hargailah Hak Cipta</b><br><br><small>Gunakan \"Lisensi Premium\" untuk menghapus credit.</small><br><br><small>#ERR_COPYRIGHT</small>', '</b><br><br><small>#ERR_DOMAIN</small>', 'onload', 'each', 'data-src', '//www.', '2iqTebY', '//api.', 'toUpperCase', 'slice', 'getElementById', '#highlight', '\" target=\"_blank\"><b>xxxxxxxxxxxxxxx</b></a>', '395073VOQlMm', '42bzEWdH', '[data-bg]', '861435YlfeWZ', 'url(', '\" target=\"_blank\" style=\"opacity:.7;font-size:70%;letter-spacing:1px;\">xxxxxxxxxxx</a>                </div>            </div>        ', 'https://www.google.com/s2/favicons?domain=', 'style', 'replace', '206934BfTClw', 'detail', 'src', '3268600YazBGv', 'XXX', '<b>Kode Lisensi</b> yang digunakan tidak valid.</b><br><br><small>#ERR_KEY</small>', 'setAttribute', 'getFullYear', 'replaceAll', 'originalEvent', 'wheelDelta', '1917558uCZcLS', 'attr', ' &#8212; Made with. <a href=\"zzzzzzzzzzzzzzzz', 'innerHTML', 'load', 'DOMMouseScroll mousewheel', 'scrollLeft', '20VIXAgk', 'split', 'charAt', 'data-bg', 'removeAttr', 'bind', 'indexOf', '3xPgKCw', 'linkpage-blogger-template', '            <link href=\"https://fonts.googleapis.com/css2?family=Rubik:wght@400;500&display=swap\" rel=\"stylesheet\">            <style>hr { margin:0!important; } body { padding: 0px!important; } * { font-family: \"Rubik\", sans-serif; font-weight:400; box-sizing:border-box;margin:0;padding:0; text-decoration:none; color:#636363; } b { font-weight: 500; color:#232323; }</style>            <div id=\"notice_html\" style=\"display:flex;background:#fafafa;min-height:100vh;text-align:center;\">                <div style=\"margin:auto;width:480px;max-width:80%;background:white;padding:30px;border-radius:10px;border:1px solid #ddd;\">                    <svg style=\"fill:#636363;width:100px;height:100px;display:block;margin:0 auto 20px\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 512 512\"><path d=\"M228.9 79.9L51.8 403.1C40.6 423.3 55.5 448 78.9 448h354.3c23.3 0 38.2-24.7 27.1-44.9L283.1 79.9c-11.7-21.2-42.5-21.2-54.2 0zM273.6 214L270 336h-28l-3.6-122h35.2zM256 402.4c-10.7 0-19.1-8.1-19.1-18.4s8.4-18.4 19.1-18.4 19.1 8.1 19.1 18.4-8.4 18.4-19.1 18.4z\"/></svg>                    <h2><b>Peringatan</b> :</h2>                    <br>                    <p>', 'hostname', 'undefined', '<img src=\"', 'preventDefault', 'body', '[data-src]', '<b>Kode Lisensi</b> hanya bisa digunakan untuk <b>', '#socmed a, #custom a'];
+    _0x4ef1 = function () {
+        return _0x1fb51e;
+    };
+    return _0x4ef1();
+}
+var _0x3f9648 = _0x1122;
+
+function _0x1122(_0x5f0a10, _0x7ca50b) {
+    var _0x4ef10b = _0x4ef1();
+    return _0x1122 = function (_0x112233, _0x495f44) {
+        _0x112233 = _0x112233 - 0x1c1;
+        var _0x59e633 = _0x4ef10b[_0x112233];
+        return _0x59e633;
+    }, _0x1122(_0x5f0a10, _0x7ca50b);
+}(function (_0x3dd883, _0x4a4795) {
+    var _0x5269f1 = _0x1122,
+        _0x1789f4 = _0x3dd883();
+    while (!![]) {
+        try {
+            var _0x517e33 = parseInt(_0x5269f1(0x1da)) / 0x1 * (-parseInt(_0x5269f1(0x1d3)) / 0x2) + -parseInt(_0x5269f1(0x1fc)) / 0x3 * (parseInt(_0x5269f1(0x1cb)) / 0x4) + parseInt(_0x5269f1(0x1dd)) / 0x5 + parseInt(_0x5269f1(0x1e3)) / 0x6 * (-parseInt(_0x5269f1(0x1db)) / 0x7) + parseInt(_0x5269f1(0x1e6)) / 0x8 + -parseInt(_0x5269f1(0x1ee)) / 0x9 * (-parseInt(_0x5269f1(0x1f5)) / 0xa) + -parseInt(_0x5269f1(0x1c9)) / 0xb;
+            if (_0x517e33 === _0x4a4795) break;
+            else _0x1789f4['push'](_0x1789f4['shift']());
+        } catch (_0x240dd9) {
+            _0x1789f4['push'](_0x1789f4['shift']());
+        }
+    }
+}(_0x4ef1, 0x397cc));
+const $license_item = _0x3f9648(0x1fd);
+license();
+
+function license() {
+    var _0x48f346 = _0x3f9648;
+
+    function _0x2e06b5(_0xa4c2b9) {
+        var _0x3b89ae = _0x1122;
+
+        function _0x13068c(_0x35cb76) {
+            try {
+                return btoa(atob(_0x35cb76)) == _0x35cb76;
+            } catch (_0x4d42b1) {
+                return ![];
+            }
+        }
+        if (_0x13068c(_0xa4c2b9)) {
+            _0xa4c2b9 = atob(_0xa4c2b9), _0xa4c2b9 = _0xa4c2b9[_0x3b89ae(0x1eb)](_0x3b89ae(0x1e7), '.'), _0xa4c2b9 = _0xa4c2b9[_0x3b89ae(0x1eb)]('YY', '-'), _0xa4c2b9 = _0xa4c2b9['replaceAll']('O5', 'a'), _0xa4c2b9 = _0xa4c2b9[_0x3b89ae(0x1eb)]('E4', 'i'), _0xa4c2b9 = _0xa4c2b9['replaceAll']('U3', 'u'), _0xa4c2b9 = _0xa4c2b9[_0x3b89ae(0x1eb)]('I2', 'e'), _0xa4c2b9 = _0xa4c2b9['replaceAll']('A1', 'o');
+
+            function _0x145659(_0x3a808c) {
+                var _0x3b4b9 = _0x3b89ae;
+                const _0x368fb4 = _0x3a808c[_0x3b4b9(0x1f6)](''),
+                    _0x5d6b45 = _0x368fb4['reverse'](),
+                    _0xf04331 = _0x5d6b45['join']('');
+                return _0xf04331;
+            }
+            return _0xa4c2b9 = _0x145659(_0xa4c2b9), _0xa4c2b9;
+        } else return '';
+    }
+
+    function _0x2d5e3a(_0x197e1a) {
+        var _0x4249e6 = _0x1122,
+            _0x22ffb0 = _0x4249e6(0x1fe) + _0x197e1a + '</p>                    <br>                    <hr style=\"border:0;border-top:1px solid #ddd;\">                    <br>                    <a href=\"https://lapak.kangrian.net/' + $license_item + _0x4249e6(0x1df);
+        return document[_0x4249e6(0x1c2)][_0x4249e6(0x1f1)] = _0x22ffb0;
+    }
+    if (typeof $license_key === _0x48f346(0x200)) _0x2d5e3a('Dilarang menghapus variable <b>$license_key</b>..');
+    else {
+        if (typeof $license_item === _0x48f346(0x200)) _0x2d5e3a('Dilarang menghapus variable <b>$license_item</b>..');
+        else {
+            if (location['hostname'][_0x48f346(0x1fb)]('localhost') != -0x1 || location[_0x48f346(0x1ff)]['indexOf'](_0x48f346(0x1ca)) != -0x1 || location[_0x48f346(0x1ff)][_0x48f346(0x1fb)](_0x48f346(0x1c7)) != -0x1) {
+                console['log']('Developed by. https://kangrian.net ( Free / Trial Version )');
+                var _0x1ab2da = document[_0x48f346(0x1d7)]('copyright');
+                if (_0x1ab2da === null) _0x2d5e3a(_0x48f346(0x1cd));
+                else {
+                    var _0x2576df = new Date()[_0x48f346(0x1ea)]();
+                    _0x1ab2da['innerHTML'] = '&#0169;' + _0x2576df + _0x48f346(0x1f0) + $license_item + _0x48f346(0x1d9), _0x1ab2da[_0x48f346(0x1e9)](_0x48f346(0x1e1), 'display:inline-block!important;opacity:1!important;position:static!important;visibility:visible!important;text-indent:0px!important;color:currentColor!important');
+                }
+            } else {
+                if ($license_key === '') _0x2d5e3a(_0x48f346(0x1c8));
+                else {
+                    const _0x58ef81 = _0x2e06b5($license_key)[_0x48f346(0x1f6)]('|')[0x0],
+                        _0x9b9e21 = _0x2e06b5($license_key)['split']('|')[0x1];
+                    if (_0x58ef81 == '' || _0x58ef81 == undefined) _0x2d5e3a(_0x48f346(0x1e8));
+                    else {
+                        if (location[_0x48f346(0x1ff)]['indexOf'](_0x58ef81) == -0x1) _0x2d5e3a('<b>Kode Lisensi</b> hanya bisa digunakan pada website dengan domain : <b>' + _0x58ef81 + _0x48f346(0x1ce));
+                        else {
+                            if (_0x9b9e21 != $license_item) {
+                                function _0x3c8af2(_0x2f1ee8) {
+                                    var _0xe16a37 = _0x48f346;
+                                    _0x2f1ee8 = _0x2f1ee8[_0xe16a37(0x1f6)](' ');
+                                    for (var _0x269596 = 0x0; _0x269596 < _0x2f1ee8['length']; _0x269596++) {
+                                        _0x2f1ee8[_0x269596] = _0x2f1ee8[_0x269596][_0xe16a37(0x1f7)](0x0)[_0xe16a37(0x1d5)]() + _0x2f1ee8[_0x269596][_0xe16a37(0x1d6)](0x1);
+                                    }
+                                    return _0x2f1ee8 = _0x2f1ee8['join'](' '), _0x2f1ee8;
+                                }
+                                _0x2d5e3a(_0x48f346(0x1c4) + _0x3c8af2($license_item[_0x48f346(0x1eb)]('-', ' ')) + '</b><br><br><small>#ERR_ITEM</small>');
+                            } else console['log']('Developed by. https://kangrian.net ( Premium Version )');
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+window[_0x3f9648(0x1cf)] = function () {
+    etc(), custom_js();
+};
+
+function etc() {
+    var _0x35a5b9 = _0x3f9648;
+    $(_0x35a5b9(0x1c3))['each'](function () {
+        var _0x26ef58 = _0x35a5b9,
+            _0x3c8c9f = $(this),
+            _0x513a5c = $(this)[_0x26ef58(0x1ef)](_0x26ef58(0x1d1));
+        _0x3c8c9f[_0x26ef58(0x1ef)]('src', _0x513a5c), $(_0x26ef58(0x201) + _0x513a5c + '\"/>')['on'](_0x26ef58(0x1f2), function () {
+            var _0x42d400 = _0x26ef58;
+            _0x3c8c9f[_0x42d400(0x1f9)](_0x42d400(0x1d1));
+        });
+    }), $(_0x35a5b9(0x1dc))[_0x35a5b9(0x1d0)](function () {
+        var _0x2f9e69 = _0x35a5b9,
+            _0x475845 = $(this)[_0x2f9e69(0x1ef)](_0x2f9e69(0x1f8));
+        $(this)['css']('background-image', _0x2f9e69(0x1de) + _0x475845 + ')')['removeAttr']('data-bg');
+    }), $(_0x35a5b9(0x1c5))['each'](function () {
+        var _0x50fa47 = _0x35a5b9,
+            _0x4c5d56 = '//' + $(this)[_0x50fa47(0x1ef)]('href')[_0x50fa47(0x1f6)]('/')[0x2];
+        _0x4c5d56 = _0x4c5d56[_0x50fa47(0x1e2)](_0x50fa47(0x1d2), '')[_0x50fa47(0x1e2)](_0x50fa47(0x1cc), '')[_0x50fa47(0x1e2)](_0x50fa47(0x1d4), '')[_0x50fa47(0x1e2)]('//web.', '')[_0x50fa47(0x1e2)]('//', ''), $('img', this)[_0x50fa47(0x1ef)](_0x50fa47(0x1e5), _0x50fa47(0x1e0) + _0x4c5d56 + _0x50fa47(0x1c6));
+    }), $(_0x35a5b9(0x1d8))[_0x35a5b9(0x1d0)](function () {
+        var _0x48eb32 = _0x35a5b9,
+            _0x2941b2 = 0x78;
+        $(this)[_0x48eb32(0x1fa)](_0x48eb32(0x1f3), function (_0x34d3e7) {
+            var _0x14c265 = _0x48eb32,
+                _0x1baa63 = _0x34d3e7[_0x14c265(0x1ec)],
+                _0x1c10a0 = _0x1baa63[_0x14c265(0x1e4)] ? _0x1baa63[_0x14c265(0x1e4)] * -_0x2941b2 : _0x1baa63[_0x14c265(0x1ed)],
+                _0x21585f = $(this)[_0x14c265(0x1f4)]();
+            _0x21585f += _0x1c10a0 > 0x0 ? -_0x2941b2 : _0x2941b2, $(this)[_0x14c265(0x1f4)](_0x21585f), _0x34d3e7[_0x14c265(0x1c1)]();
+        });
+    });
+}
